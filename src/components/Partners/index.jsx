f08937646f0e9138a17330ed078145870b96e92c
Emran <nightsking12345@gmail.com>
2024-09-09 17:53:17 +0500

    Add branch develop and Fiexd Error page && Trusted by

--- src/components/Partners/index.jsx
+++ src/components/Partners/index.jsx
@@ -1,4 +1,5 @@
-import { useEffect, useState } from 'react'
+/* eslint-disable react/prop-types */
+import { useMemo } from 'react'
 import Slider from 'react-slick'
 import 'slick-carousel/slick/slick-theme.css'
 import 'slick-carousel/slick/slick.css'
@@ -20,54 +21,45 @@ const images = [
 	[books, nature, emplas, sky, google, clues],
 ]
 
+const ImageSlider = ({ imageSet, speed }) => {
+	const settings = useMemo(
+		() => ({
+			speed: 2500,
+			infinite: true,
+			autoplay: true,
+			autoplaySpeed: speed,
+			slidesToShow: 1,
+			slidesToScroll: 1,
+			vertical: true,
+			verticalSwiping: true,
+			cssEase: 'ease-in-out',
+			arrows: false,
+			pauseOnHover: false,
+			draggable: false, // Prevent dragging
+			swipe: false, // Disable swiping
+			touchMove: false, // Disable touch interaction
+		}),
+		[speed]
+	)
+
+	return (
+		<Slider {...settings}>
+			{imageSet.map((src, index) => (
+				<div key={index} className={styles.sliderItem}>
+					<img src={src} alt={`partner-${index}`} />
+				</div>
+			))}
+		</Slider>
+	)
+}
+
 export default function Partners() {
-	const [slideIndexes, setSlideIndexes] = useState(Array(5).fill(0))
-
-	const settings = speed => ({
-		speed: 2500,
-		infinite: true,
-		autoplay: true,
-		autoplaySpeed: speed,
-		slidesToShow: 1,
-		slidesToScroll: 1,
-		vertical: true,
-		verticalSwiping: true,
-		swipeToSlide: true,
-		cssEase: 'ease-in-out',
-		arrows: false,
-		pauseOnHover: false,
-	})
-
-	useEffect(() => {
-		const intervals = slideIndexes.map((_, i) =>
-			setInterval(() => {
-				setSlideIndexes(prevIndexes => {
-					const newIndexes = [...prevIndexes]
-					newIndexes[i] = (newIndexes[i] + 1) % images[i].length
-					return newIndexes
-				})
-			}, 1500 + i * 500)
-		)
-
-		return () => intervals.forEach(interval => clearInterval(interval))
-	}, [])
-
 	return (
 		<section className={styles.partners}>
 			<h4 className={styles.partners_trustedText}>Trusted by</h4>
 			<div className={styles.framecon}>
 				{images.map((imageSet, i) => (
-					<Slider key={i} {...settings(2000 + i * 500)}>
-						{imageSet.map((src, index) => (
-							<div
-								key={index}
-								style={{ paddingTop: '40px' }}
-								className={styles.sliderItem}
-							>
-								<img src={src} alt='' />
-							</div>
-						))}
-					</Slider>
+					<ImageSlider key={i} imageSet={imageSet} speed={2000 + i * 500} />
 				))}
 			</div>
 		</section>
