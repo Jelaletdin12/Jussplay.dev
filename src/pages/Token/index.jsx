051cf4f59a9f796bef1b9b0a8112785835ce9f88
Emran <nightsking12345@gmail.com>
2024-09-24 20:15:24 +0500

    Fixed bug

--- src/pages/Token/index.jsx
+++ src/pages/Token/index.jsx
@@ -1,129 +1,122 @@
-import bg from "../../assets/blockchain/01.png";
-import icon1 from "../../assets/token/ic1.png";
-import icon2 from "../../assets/token/ic2.png";
-import icon3 from "../../assets/token/ic3.png";
-import icon4 from "../../assets/token/ic4.png";
-import { HeaderBlockchain } from "../../components/Blockchain/HeaderBlockchain";
-import Footer from "../../components/Footer/index";
-import Header from "../../components/Header";
-import { CustomScroll } from "../../components/scroll/CustomScroll";
-import Features from "../../components/Token/Features";
-import NftCards from "../../components/Token/NftCards";
-import Services from "../../components/Token/services";
+import bg from '../../assets/blockchain/01.png'
+import icon1 from '../../assets/token/ic1.png'
+import icon2 from '../../assets/token/ic2.png'
+import icon3 from '../../assets/token/ic3.png'
+import icon4 from '../../assets/token/ic4.png'
+import { HeaderBlockchain } from '../../components/Blockchain/HeaderBlockchain'
+import Footer from '../../components/Footer/index'
+import Header from '../../components/Header'
+import { CustomScroll } from '../../components/scroll/CustomScroll'
+import Features from '../../components/Token/Features'
+import NftCards from '../../components/Token/NftCards'
+import Services from '../../components/Token/services'
 import {
-<<<<<<< HEAD
 	subTitleNFTTokenHeaderBlockchain,
 	titleNFTTokenHeaderBlockchain,
 } from '../../components/Util/dataBlockchainPage'
+import transition from '../../pageTransition'
 import styles from './token.module.scss'
-=======
-  subTitleNFTTokenHeaderBlockchain,
-  titleNFTTokenHeaderBlockchain,
-} from "../../components/Util/contentBlockchainPage";
-import styles from "./token.module.scss";
-import transition from "../../pageTransition";
->>>>>>> 552debf79814685cea981f31216085d131f1ef85
 
 const token = () => {
-  return (
-    <CustomScroll>
-      <div className={styles.appContainer}>
-        <Header />
-        <HeaderBlockchain
-          title={titleNFTTokenHeaderBlockchain}
-          subtitle={subTitleNFTTokenHeaderBlockchain}
-        />
-        <Services />
-        <NftCards />
-        <Features />
-        <div className={styles.frame}>
-          <div className={styles.header}>
-            <h2>Non Fungible Token and Blockchain Relationship</h2>
-            <p>
-              Do not confuse the NFT with crypto. The only thing they have in
-              common is Blockchain. Have a look at it how ?
-            </p>
-          </div>
-          <div className={styles.service}>
-            <div className={styles.items}>
-              <img src={icon3} alt="" />
-              <div className={styles.itemsContent}>
-                <p>
-                  NFT is stored securely in Blockchain, which means the tokens
-                  can never be removed, destroyed, or replicated no matter what.
-                </p>
-              </div>
-            </div>
-            <div className={styles.items}>
-              <img src={icon4} alt="" />
-              <div className={styles.itemsContent}>
-                <p>
-                  NFT is stored securely in Blockchain, which means the tokens
-                  can never be removed, destroyed, or replicated no matter what.
-                </p>
-              </div>
-            </div>
-            <div className={styles.items}>
-              <img src={icon2} alt="" />
-              <div className={styles.itemsContent}>
-                <p>
-                  NFT is stored securely in Blockchain, which means the tokens
-                  can never be removed, destroyed, or replicated no matter what.
-                </p>
-              </div>
-            </div>
-            <div className={styles.items}>
-              <img src={icon1} alt="" />
-              <div className={styles.itemsContent}>
-                <p>
-                  NFT is stored securely in Blockchain, which means the tokens
-                  can never be removed, destroyed, or replicated no matter what.
-                </p>
-              </div>
-            </div>
-            <div className={styles.items}>
-              <img src={icon3} alt="" />
-              <div className={styles.itemsContent}>
-                <p>
-                  NFT is stored securely in Blockchain, which means the tokens
-                  can never be removed, destroyed, or replicated no matter what.
-                </p>
-              </div>
-            </div>
-            <div className={styles.items}>
-              <img src={icon4} alt="" />
-              <div className={styles.itemsContent}>
-                <p>
-                  NFT is stored securely in Blockchain, which means the tokens
-                  can never be removed, destroyed, or replicated no matter what.
-                </p>
-              </div>
-            </div>
-            <div className={styles.items}>
-              <img src={icon2} alt="" />
-              <div className={styles.itemsContent}>
-                <p>
-                  NFT is stored securely in Blockchain, which means the tokens
-                  can never be removed, destroyed, or replicated no matter what.
-                </p>
-              </div>
-            </div>
-            <div className={styles.items}>
-              <img src={icon1} alt="" />
-              <div className={styles.itemsContent}>
-                <p>
-                  NFT is stored securely in Blockchain, which means the tokens
-                  can never be removed, destroyed, or replicated no matter what.
-                </p>
-              </div>
-            </div>
-          </div>
-        </div>
-        <img className={styles.bgImg} src={bg} alt="" />
-        <Footer />
-      </div>
-    </CustomScroll>
-  );
-};
+	return (
+		<CustomScroll>
+			<div className={styles.appContainer}>
+				<Header />
+				<HeaderBlockchain
+					title={titleNFTTokenHeaderBlockchain}
+					subtitle={subTitleNFTTokenHeaderBlockchain}
+				/>
+				<Services />
+				<NftCards />
+				<Features />
+				<div className={styles.frame}>
+					<div className={styles.header}>
+						<h2>Non Fungible Token and Blockchain Relationship</h2>
+						<p>
+							Do not confuse the NFT with crypto. The only thing they have in
+							common is Blockchain. Have a look at it how ?
+						</p>
+					</div>
+					<div className={styles.service}>
+						<div className={styles.items}>
+							<img src={icon3} alt='' />
+							<div className={styles.itemsContent}>
+								<p>
+									NFT is stored securely in Blockchain, which means the tokens
+									can never be removed, destroyed, or replicated no matter what.
+								</p>
+							</div>
+						</div>
+						<div className={styles.items}>
+							<img src={icon4} alt='' />
+							<div className={styles.itemsContent}>
+								<p>
+									NFT is stored securely in Blockchain, which means the tokens
+									can never be removed, destroyed, or replicated no matter what.
+								</p>
+							</div>
+						</div>
+						<div className={styles.items}>
+							<img src={icon2} alt='' />
+							<div className={styles.itemsContent}>
+								<p>
+									NFT is stored securely in Blockchain, which means the tokens
+									can never be removed, destroyed, or replicated no matter what.
+								</p>
+							</div>
+						</div>
+						<div className={styles.items}>
+							<img src={icon1} alt='' />
+							<div className={styles.itemsContent}>
+								<p>
+									NFT is stored securely in Blockchain, which means the tokens
+									can never be removed, destroyed, or replicated no matter what.
+								</p>
+							</div>
+						</div>
+						<div className={styles.items}>
+							<img src={icon3} alt='' />
+							<div className={styles.itemsContent}>
+								<p>
+									NFT is stored securely in Blockchain, which means the tokens
+									can never be removed, destroyed, or replicated no matter what.
+								</p>
+							</div>
+						</div>
+						<div className={styles.items}>
+							<img src={icon4} alt='' />
+							<div className={styles.itemsContent}>
+								<p>
+									NFT is stored securely in Blockchain, which means the tokens
+									can never be removed, destroyed, or replicated no matter what.
+								</p>
+							</div>
+						</div>
+						<div className={styles.items}>
+							<img src={icon2} alt='' />
+							<div className={styles.itemsContent}>
+								<p>
+									NFT is stored securely in Blockchain, which means the tokens
+									can never be removed, destroyed, or replicated no matter what.
+								</p>
+							</div>
+						</div>
+						<div className={styles.items}>
+							<img src={icon1} alt='' />
+							<div className={styles.itemsContent}>
+								<p>
+									NFT is stored securely in Blockchain, which means the tokens
+									can never be removed, destroyed, or replicated no matter what.
+								</p>
+							</div>
+						</div>
+					</div>
+				</div>
+				<img className={styles.bgImg} src={bg} alt='' />
+				<Footer />
+			</div>
+		</CustomScroll>
+	)
+}
 
-export default transition(token);
+export default transition(token)
