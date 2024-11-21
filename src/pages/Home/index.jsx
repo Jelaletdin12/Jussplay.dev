e26b7aa50777cc50ce8c456cb2a893d16d9c52d3
Jelaletdin12 <jcarymuhammedow@gmail.com>
2024-11-21 21:39:29 +0500

    44 commit

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,6 +1,5 @@
 /* eslint-disable react/no-unescaped-entities */
 import gsap from "gsap";
-//import ScrollTrigger from "gsap/dist/ScrollTrigger";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 import { useEffect, useRef, useState } from "react";
 import { FaArrowTurnDown } from "react-icons/fa6";
@@ -19,177 +18,81 @@ import styles from "./home.module.scss";
 gsap.registerPlugin(ScrollTrigger);
 
 const workTabs = [
-  {
-    id: 1,
-    name: "kidstime",
-    type: "mobile app",
-    img: kidstime,
-  },
-  {
-    id: 2,
-    name: "emplas realestate",
-    type: "web",
-    isTop: true,
-    img: emplas,
-  },
-  {
-    id: 3,
-    name: "jussplay.labs",
-    type: "web",
-  },
-  {
-    id: 4,
-    name: "asghgabat drift club",
-    type: "mobile app",
-  },
-  {
-    id: 5,
-    name: "coming soon",
-  },
-  {
-    id: 6,
-    name: "coming soon",
-    isBottom: true,
-  },
+  { id: 1, name: "kidstime", type: "mobile app", img: kidstime },
+  { id: 2, name: "emplas realestate", type: "web", isTop: true, img: emplas },
+  { id: 3, name: "jussplay.labs", type: "web" },
+  { id: 4, name: "asghgabat drift club", type: "mobile app" },
+  { id: 5, name: "coming soon" },
+  { id: 6, name: "coming soon", isBottom: true },
 ];
+
 function Home() {
   const container = useRef();
   const interBubble = useRef();
-  const [flag, setFlag] = useState(false);
-  const [isTopRemoved, setIsTopRemoved] = useState(false);
 
-  useEffect(() => {
-    const handleLoad = () => {
-      ScrollTrigger.refresh();
-    };
-
-    window.addEventListener("load", handleLoad);
-
-    return () => {
-      window.removeEventListener("load", handleLoad);
-    };
-  }, []);
-
-  useEffect(() => {
-    let curX = 0;
-    let curY = 0;
-    let tgX = 0;
-    let tgY = 0;
-
-    function move() {
-      curX += (tgX - curX) / 20;
-      curY += (tgY - curY) / 20;
-      if (interBubble.current.style) {
-        interBubble.current.style.transform = `translate(${Math.round(
-          curX
-        )}px, ${Math.round(curY)}px)`;
-        requestAnimationFrame(move);
-      }
-    }
-
-    window.addEventListener("mousemove", (event) => {
-      tgX = event.clientX;
-      tgY = event.clientY;
-    });
-
-    move();
-  }, []);
-
-  const debounce = (callback, wait) => {
-    let timeout;
+  function throttle(callback, limit) {
+    let waiting = false; // Whether the callback is waiting to be invoked
     return function (...args) {
-      clearTimeout(timeout);
-      timeout = setTimeout(() => callback.apply(this, args), wait);
+      if (!waiting) {
+        callback.apply(this, args);
+        waiting = true;
+        setTimeout(() => (waiting = false), limit);
+      }
     };
-  };
+  }
 
   useEffect(() => {
-    const handleScroll = debounce(() => {
-      // Оптимизация управления флагами
-      setFlag(window.scrollY >= 790);
-      setIsTopRemoved(window.scrollY >= 1650);
-    }, 100);
+    const handleMouseMove = throttle((event) => {
+      const { clientX: x, clientY: y } = event;
+      if (interBubble.current) {
+        gsap.to(interBubble.current, {
+          x,
+          y,
+          duration: 0.2,
+          ease: "power2.out",
+        });
+      }
+    }, 100); // Adjust throttle delay if needed
 
-    window.addEventListener("scroll", handleScroll);
+    window.addEventListener("mousemove", handleMouseMove);
 
+    return () => window.removeEventListener("mousemove", handleMouseMove);
+  }, []);
+
+  useEffect(() => {
     const mm = gsap.matchMedia();
 
-    // const animateForLargeScreens = () => {
-    // 	const heroAnim = gsap.timeline({
-    // 		scrollTrigger: {
-    // 			trigger: '#hero',
-    // 			start: 'top top',
-    // 			end: 'bottom top',
-    // 			scrub: true,
-    // 		},
-    // 	})
-    // 	heroAnim.to('#heroText1', { x: -200, opacity: 0 })
-    // 	heroAnim.to('#heroText2', { x: 200, opacity: 0 }, '<=')
-    // 	heroAnim.to('#logoLottie', { x: 300 }, '<=')
-    // 	heroAnim.to('#about', { y: -550, opacity: 1 }, '<=')
-    // }
-
-    // const animateForSmallScreens = () => {
-    // 	const heroAnim = gsap.timeline({
-    // 		scrollTrigger: {
-    // 			trigger: '#hero',
-    // 			start: 'top top',
-    // 			end: 'bottom top',
-    // 			scrub: true,
-    // 			invalidateOnRefresh: true,
-    // 			refreshPriority: 1,
-    // 		},
-    // 	})
-    // 	heroAnim.to('#heroText1', { x: -300, opacity: 0 })
-    // 	heroAnim.to('#heroText2', { x: 300, opacity: 0 }, '<=')
-    // 	heroAnim.to('#logoLottie', { y: -400 }, '<=')
-    // 	heroAnim.to('#about', { y: -450, opacity: 1 }, '<=')
-    // }
-
-    // Регистрация медиазапросов
-    // mm.add('(min-width: 521px)', animateForLargeScreens)
-    // mm.add('(max-width: 520px)', animateForSmallScreens)
-
-    const createAnimation = (x1, x2, x, y, aboutY) => {
-      const heroAnim = gsap.timeline({
+    const createAnimation = ({ x1, x2, x, y, aboutY }) => {
+      const tl = gsap.timeline({
         scrollTrigger: {
           trigger: "#hero",
           start: "top top",
           end: "bottom top",
           scrub: true,
-          invalidateOnRefresh: true,
-          refreshPriority: 1,
         },
       });
-      heroAnim.to("#heroText1", { x: x1, opacity: 0 });
-      heroAnim.to("#heroText2", { x: x2, opacity: 0 }, "<");
-      heroAnim.to("#logoLottie", { x }, "<");
-      heroAnim.to("#logoLottie", { y }, ">");
-      heroAnim.to("#about", { y: aboutY, opacity: 1 }, "<");
+      tl.to("#heroText1", { x: x1, opacity: 0 })
+        .to("#heroText2", { x: x2, opacity: 0 }, "<")
+        .to("#logoLottie", { x }, "<")
+        .to("#logoLottie", { y }, ">")
+        .to("#about", { y: aboutY, opacity: 1 }, "<");
     };
 
     mm.add("(min-width: 521px)", () =>
-      createAnimation(-200, 200, 400, 0, -550)
+      createAnimation({ x1: -200, x2: 200, x: 400, y: 0, aboutY: -550 })
     );
     mm.add("(max-width: 520px)", () =>
-      createAnimation(-300, 300, 0, -400, -450)
+      createAnimation({ x1: -150, x2: 150, x: 0, y: -350, aboutY: -400 })
     );
 
-    return () => {
-      window.removeEventListener("scroll", handleScroll);
-      mm.revert();
-    };
+    return () => mm.revert();
   }, []);
 
   return (
     <main className={styles.main} ref={container}>
       <Header />
       <section
-        className={
-          isTopRemoved
-            ? styles.main_heroSection
-            : styles.main_heroSection + " " + styles.main_heroSection_active
-        }
+        className={`${styles.main_heroSection} ${styles.main_heroSection_active}`}
         id="hero"
       >
         <div className="gradient-bg">
@@ -226,7 +129,7 @@ function Home() {
               JUST SAY IT
             </div>
             <div id="logoLottie">
-              <LogoLottie></LogoLottie>
+              <LogoLottie />
             </div>
             <div
               id="heroText2"
@@ -236,13 +139,7 @@ function Home() {
             </div>
           </h1>
         </div>
-        <section
-          className={
-            styles.main_intro + " " + (flag && styles.main_intro_onhover)
-          }
-          id="about"
-        >
-          {" "}
+        <section className={styles.main_intro} id="about">
           <div>
             <p>
               Join <span>JussPlay,</span>
@@ -251,7 +148,6 @@ function Home() {
               and see your project <span>JUST</span> come to life.
             </p>
             <p>
-              {" "}
               <span>SAY</span> what you want: web, mobile, design, or
             </p>
             <p>
@@ -262,12 +158,11 @@ function Home() {
               <span>WE</span> manage every project professionally.
             </p>
             <p>
-              Our principal is to <span>MAKE</span> your vision a reality.{" "}
+              Our principal is to <span>MAKE</span> your vision a reality.
             </p>
             <p>Trust us with your ideas,</p>
             <p>
-               and we will complete <span>IT</span> to
-              perfection.
+              and we will complete <span>IT</span> to perfection.
             </p>
           </div>
         </section>
@@ -295,7 +190,6 @@ function Home() {
           ))}
         </div>
       </section>
-
       <ScrollSection />
       <Partners />
       <Footer />
