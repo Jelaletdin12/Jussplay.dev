f363c13acca18f58345417f9de1ea212357f5285
Ahmet <ahmetsaparow.mr@gmail.com>
2024-12-02 11:20:35 +0500

    About Us finished

--- src/pages/About/index.jsx
+++ src/pages/About/index.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef } from "react";
+import { useRef } from "react";
 import { HashLink } from "react-router-hash-link";
 import styles from "./about.module.scss";
 import Footer from "../../components/Footer";
@@ -10,79 +10,41 @@ import blue from "../../assets/blue.png";
 import transition from "../../pageTransition";
 import Header from "../../components/Header";
 import { IoIosArrowForward } from "react-icons/io";
-import { FaArrowRightLong } from "react-icons/fa6";
-import Typewriter from "typewriter-effect/dist/core";
-import { CustomScroll } from '../../components/scroll/CustomScroll'
 
+import chevronRight from "../../assets/webapp/chevronRight.png";
+import chevronLeft from "../../assets/webapp/chevronLeft.png";
+import person from "../../assets/webapp/person.png";
+
+import { Form } from "../../components/Blockchain/Form";
+
+import { useMediaQuery } from "react-responsive";
+
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Autoplay, Navigation } from "swiper/modules";
+import "swiper/css";
+import "swiper/css/navigation";
+
+const timelineTitles = [
+  "Establishment of Jussplay",
+  "The Leap Forward",
+  "The First Major Milestone",
+  "The First Major Milestone",
+];
+
+const values = [
+  "Efficiency",
+  "Results-driven",
+  "Client-Centric",
+  "Creativity",
+  "Innovation",
+  "Adaptability",
+];
 
 const About = () => {
-  const journeyRef = useRef(null);
-  const visionRef = useRef(null);
-  const visionTextRef = useRef(null);
-  const foundationRef = useRef(null);
-  const foundationTextRef = useRef(null);
-  const milestonesRef = useRef(null);
-  const milestonesTextRef = useRef(null);
-  const primaryAreaRef = useRef(null);
-  const primaryAreaTextRef = useRef(null);
-  const visionFutureRef = useRef(null);
-  const visionFutureTextRef = useRef(null);
-  // const partnerButtonRef = useRef(null);
-  // const primaryButtonRef = useRef(null);
+  const tablet = useMediaQuery({ query: "(max-width: 768px)" });
 
-  useEffect(() => {
-    const typewriterConfig = {
-      loop: false,
-      delay: 75,
-      cursor: "|",
-    };
-
-    const createTypewriter = (ref, text) => {
-      if (ref.current) {
-        const typewriter = new Typewriter(ref.current, typewriterConfig);
-        typewriter
-          .typeString(text)
-          .callFunction(() => {
-            const cursor = ref.current.querySelector(".Typewriter__cursor");
-            if (cursor) {
-              cursor.style.display = "none";
-            }
-          })
-          .start();
-      }
-    };
-
-    createTypewriter(journeyRef, "Start Journey");
-    createTypewriter(visionRef, "Company vision");
-    createTypewriter(foundationRef, "Foundation of Jussplay");
-    createTypewriter(milestonesRef, "Key milestones");
-    createTypewriter(primaryAreaRef, "Company primary area");
-    createTypewriter(visionFutureRef, "Company’s vision for future");
-
-    createTypewriter(
-      visionTextRef,
-      "Our company's mission is to bring technology at people’s fingertips where there are no limitations on their dream softwares to turn into reality. To provide the best service all across the world. Help businesses exponentially grow with our service and break the barrier between technology and businesses."
-    );
-    createTypewriter(
-      foundationTextRef,
-      "Shokran Ahadi is a tech enthusiast who has been in the industry for more than 7 years. The Company was founded in September 2023 to solve a leading problem where businesses, individuals, and entrepreneurs had barriers into stepping out in the tech industry and having their hands on their wished software."
-    );
-    createTypewriter(
-      milestonesTextRef,
-      "Jussplay has made significant progress in providing the best services, including reaching into complex software domains such as blockchain and AI. Jussplay has achieved this through great management and a highly skilled professional team. The company, which started as a startup, is now a global business, fulfilling client needs and satisfaction."
-    );
-    createTypewriter(
-      primaryAreaTextRef,
-      "Jussplay currently operates in Europe, Central Asia, and is looking to expand into the western markets (US)."
-    );
-    createTypewriter(
-      visionFutureTextRef,
-      "Jussplay sees itself as a leading tech company across the world with international services. It envisions itself as a leading tech development company deeply rooted in technology and the future of technology."
-    );
-
-    // createTypewriter(primaryButtonRef, "Work with us");
-    // createTypewriter(partnerButtonRef, "Become Jussplay partner");
-  }, []);
+  const prevRef = useRef(null);
+  const nextRef = useRef(null);
 
   return (
     <>
@@ -91,14 +53,11 @@ const About = () => {
         <div className={styles.upside}>
           <div className={styles.contentWrapper}>
             <h1 className={styles.heading}>About Jussplay</h1>
-              <HashLink
-                to={"#startJourney"}
-                smooth
-              >
-                <button className={styles.button}>
+            <HashLink to={"#startJourney"} smooth>
+              <button className={styles.button}>
                 Meet with us <IoIosArrowForward />
-            </button>
-              </HashLink>{" "}
+              </button>
+            </HashLink>{" "}
           </div>
           <div className={styles.rightColumn}>
             <div className={styles.bubbleContainer}>
@@ -120,57 +79,188 @@ const About = () => {
             </div>
           </div>
         </div>
-        <div className={styles.bottom} id="startJourney">
-          <h1 ref={journeyRef}></h1>
-          <div className={styles.addContent}>
-            <div className={styles.vision}>
-              <div className={styles.foundation}>
-                <h1 ref={visionRef}></h1>
-                <p ref={visionTextRef}></p>
-              </div>
-              <div className={styles.foundation} style={{ textAlign: "right" }}>
-                <h1 ref={foundationRef}></h1>
-                <p ref={foundationTextRef}></p>
-              </div>
-              <div className={styles.foundation}>
-                <h1 ref={milestonesRef}></h1>
-                <p ref={milestonesTextRef}></p>
-              </div>
-            </div>
-            <div className={styles.primary}>
-              <div className={styles.primaryArea}>
-                <h1 ref={primaryAreaRef}></h1>
-                <p ref={primaryAreaTextRef}></p>
-                <button>
-                  Work with us <FaArrowRightLong />
-                </button>
-              </div>
-              <div className={styles.visionFuture}>
-                <h1 ref={visionFutureRef} style={{ color: "#121212" }}></h1>
-                <p
-                  ref={visionFutureTextRef}
-                  style={{ color: "rgba(0, 0, 0, 0.65)" }}
-                ></p>
-                <button>
-                  Become Jussplay partner <FaArrowRightLong />
-                </button>
-              </div>
-            </div>
+
+        <div className={styles.bigTContainer}>
+          <div className={styles.bigT}>Our Story</div>
+          <p className={styles.regularP}>
+            At Jussplay Developments, we pride ourselves on our commitment to
+            excellence and our dedication to our clients' success. We believe
+            that collaboration and communication are key to success, which is
+            why we prioritize transparency and open communication with our
+            clients
+          </p>
+        </div>
+        <div className={styles.timeline}>
+          <div className={styles.years}>
+            <div className={styles.year}>2021</div>
+            <div className={styles.year}>2022</div>
+            <div className={styles.year}>2023</div>
+            <div className={styles.year}>2024</div>
           </div>
-          <div className={styles.statistics}>
-            <div className={styles.statItem}>
-              <h1>In 2023</h1>
-              <p>Company founded</p>
-            </div>
-            <div className={styles.statItem}>
-              <h1>20+</h1>
-              <p>Active working employees</p>
-            </div>
-            <div className={styles.statItem}>
-              <h1>15+</h1>
-              <p>Jussplay offers more than 15 services</p>
-            </div>
+          <div className={styles.indicator}>
+            <div className={styles.firstLine}></div>
+            <div className={styles.circle}></div>
+            <div className={styles.line}></div>
+            <div className={styles.circle}></div>
+            <div className={styles.line}></div>
+            <div className={styles.circle}></div>
+            <div className={styles.line}></div>
+            <div className={styles.circle}></div>
+            <div className={styles.lastLine}></div>
           </div>
+          <div className={styles.textBlocks}>
+            {timelineTitles.map((t, i) => {
+              return (
+                <div key={i} className={styles.textBlock}>
+                  {tablet ? <div className={styles.year}>2021</div> : null}
+                  <div className={styles.biggerText}>{t}</div>
+                  <p className={styles.regularP}>
+                    At Jussplay Developments, we pride ourselves on our
+                    commitment to excellence and our dedication to our clients'
+                    success. We believe that collaboration and communication are
+                    key to success, which is why we prioritize transparency and
+                    open communication with our clients
+                  </p>
+                </div>
+              );
+            })}
+          </div>
+        </div>
+
+        <div className={styles.values}>
+          <div className={styles.bigTContainer}>
+            <div className={styles.bigT}>Our values</div>
+            <p className={styles.regularP}>
+              At ADSORB, we pride ourselves on our commitment to excellence and
+              our dedication to our clients' success. We believe that
+              collaboration and communication are key to success, which is why
+              we prioritize transparency and open communication with our clients
+            </p>
+          </div>
+          <div className={styles.valuesWrapper}>
+            {values.map((v) => {
+              return (
+                <div key={v} className={styles.valueBlock}>
+                  <div className={styles.valueGradient}></div>
+                  <div className={styles.valueTextWrapper}>
+                    <div className={styles.valueTitle}>{v}</div>
+                    <p className={styles.valueP}>
+                      We develop metaverse action and adventure gaming apps with
+                      NFT in-game assets like avtaar, assets, skins, locations,
+                      stadiums, levels, etc.
+                    </p>
+                  </div>
+                </div>
+              );
+            })}
+          </div>
+        </div>
+        <div className={styles.bigTContainer}>
+          <div
+            className={styles.bigT}
+            style={{
+              textTransform: "uppercase",
+            }}
+          >
+            MEET OUR TEAM
+          </div>
+          <p className={styles.regularP}>
+            At Jussplay Developments, we believe that a successful marketing
+            campaign requires the collaborative efforts of talented individuals
+            with diverse skills and expertise. Our dedicated team of
+            professionals is here to help your business thrive in the
+            ever-changing landscape of digital marketing
+          </p>
+        </div>
+        <div className={styles.swiperContainer}>
+          <div ref={prevRef}>
+            <img src={chevronLeft} alt="" />
+          </div>
+          <Swiper
+            loop
+            className={styles.swiper}
+            spaceBetween={50}
+            slidesPerView={1}
+            modules={[Autoplay, Navigation]}
+            breakpoints={{
+              768: {
+                slidesPerView: 2,
+              },
+              1000: {
+                slidesPerView: 3.5,
+              },
+            }}
+            navigation={{
+              prevEl: prevRef.current,
+              nextEl: nextRef.current,
+            }}
+            onInit={(swiper) => {
+              // Attach refs to Swiper navigation once it initializes
+              if (prevRef.current && nextRef.current) {
+                swiper.params.navigation.prevEl = prevRef.current;
+                swiper.params.navigation.nextEl = nextRef.current;
+                swiper.navigation.init();
+                swiper.navigation.update();
+              }
+            }}
+          >
+            <SwiperSlide>
+              <div className={styles.teamMemberCard}>
+                <div className={styles.teamMemberImg}>
+                  <img src={person} alt="person" />
+                </div>
+                <div className={styles.personName}>Yuliya Malysh</div>
+                <div className={styles.bioText}>View Bio</div>
+              </div>
+            </SwiperSlide>
+            <SwiperSlide>
+              <div className={styles.teamMemberCard}>
+                <div className={styles.teamMemberImg}>
+                  <img src={person} alt="person" />
+                </div>
+                <div className={styles.personName}>Yuliya Malysh</div>
+                <div className={styles.bioText}>View Bio</div>
+              </div>
+            </SwiperSlide>
+            <SwiperSlide>
+              <div className={styles.teamMemberCard}>
+                <div className={styles.teamMemberImg}>
+                  <img src={person} alt="person" />
+                </div>
+                <div className={styles.personName}>Yuliya Malysh</div>
+                <div className={styles.bioText}>View Bio</div>
+              </div>
+            </SwiperSlide>
+            <SwiperSlide>
+              <div className={styles.teamMemberCard}>
+                <div className={styles.teamMemberImg}>
+                  <img src={person} alt="person" />
+                </div>
+                <div className={styles.personName}>Yuliya Malysh</div>
+                <div className={styles.bioText}>View Bio</div>
+              </div>
+            </SwiperSlide>
+            <SwiperSlide>
+              <div className={styles.teamMemberCard}>
+                <div className={styles.teamMemberImg}>
+                  <img src={person} alt="person" />
+                </div>
+                <div className={styles.personName}>Yuliya Malysh</div>
+                <div className={styles.bioText}>View Bio</div>
+              </div>
+            </SwiperSlide>
+          </Swiper>
+          <div ref={nextRef}>
+            <img src={chevronRight} alt="" />
+          </div>
+        </div>
+
+        <div className={styles.contactBlock}>
+          <p>
+            With Jussplay, you can trust that you're working with a team of
+            experts who are dedicated to helping your business succeed.
+          </p>
+          <Form />
         </div>
       </section>
       <Footer />
